f90613999ffc20febee0a45859e8a78273bee998
idiancapozzoli <56178323+idiancapozzoli@users.noreply.github.com>
2019-11-06 22:41:48 -0300

    Segunda versão do BDD
    Foi sugerido incrementar o banco de dados e inserir novas tabelas de relacionamento

---
+++ bddGeomatic.sql
@@ -0,0 +1,43 @@
+CREATE DATABASE Forum;
+use Forum;
+
+create table tipoUsuario(
+codTipo int not null primary key,
+nomeTipo varchar(100)
+)engine innodb;
+
+create table categoria(
+codCategoria int not null primary key,
+nomeCategoria varchar(100)
+)engine innodb;
+
+create table usuario(
+codUsuario int not null primary key,
+nome varchar(100),
+nick varchar (100),
+senha varchar(20),
+codTipo int,
+email varchar(100),
+telefone varchar(15),
+foreign key (codTipo) references tipoUsuario(codTipo)
+)engine innodb;
+
+create table topico(
+codTopico int not null primary key,
+textoTopico varchar(500),
+dataTopico date,
+codCategoria int,
+codUsuario int,
+foreign key (codCategoria) references categoria(codCategoria),
+foreign key (codUsuario) references usuario(codUsuario)
+)engine innodb;
+
+create table comentario(
+codComentario int not null primary key,
+textoComentario varchar(500),
+dataComentario date,
+codTopico int,
+codUsuario int,
+foreign key (codTopico) references topico(codTopico),
+foreign key (codUsuario) references usuario(codUsuario)
+)engine innodb;
